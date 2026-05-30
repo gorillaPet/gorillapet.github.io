342b869030ee1a11479cbccace12dfdf6dae2fce
Nick <gorillapet@proton.me>
2026-05-30 18:44:06 +1000

    Update ticker.js

--- ticker.js
+++ ticker.js
@@ -29,10 +29,10 @@ document.addEventListener("DOMContentLoaded", () => {
 
   // BOTTOM TICKER
 const bottomMessages = [
-  "If you're reading this it's likely because I am applying for a job or an internship with you, in which case - it's your lucky day." 
-  "I once did a deep barbell squatm of 180kg. I bet my resume didn't mention that. Or my 210kg deadlift. When you hire your next grad, do you want some chicken legged "
-  "weakling, or someone who can wrench the elevator doors open if we get stuck in there? I know you're not allowed to admit that kind of thing " 
-  "with modern workplace laws, and you're not even allowed to ask, but lets be real here. If burglars break into the office, other grads will be using Dijkstra's to "
+  "If you're reading this it's likely because I am applying for a job or an internship with you, in which case - it's your lucky day.", 
+  "I once did a deep barbell squatm of 180kg. I bet my resume didn't mention that. Or my 210kg deadlift. When you hire your next grad, do you want some chicken legged ",
+  "weakling, or someone who can wrench the elevator doors open if we get stuck in there? I know you're not allowed to admit that kind of thing ", 
+  "with modern workplace laws, and you're not even allowed to ask, but lets be real here. If burglars break into the office, other grads will be using Dijkstra's to ",
   "find the closest emergency exit. You have friends and family who care about you - don't gamble with your future and hire the guy who will stick it out when it gets tough."
 ];
   const combinedBottom = bottomMessages.join(" • ");
