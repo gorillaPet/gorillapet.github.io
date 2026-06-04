61b448341d53556a2ddad3994aeedfea9b8a7d1e
Nick <gorillapet@proton.me>
2026-06-05 00:26:23 +1000

    Massive overhaul
    Fixing draft information, adding image lightboxes, overall giving  the projects more direction

---
+++ lightbox.js
@@ -0,0 +1,14 @@
+function openLightbox(src) {
+    document.getElementById('lightbox-img').src = src;
+    document.getElementById('lightbox').classList.add('open');
+    document.body.style.overflow = 'hidden';
+}
+
+function closeLightbox() {
+    document.getElementById('lightbox').classList.remove('open');
+    document.body.style.overflow = '';
+}
+
+document.addEventListener('keydown', function(e) {
+    if (e.key === 'Escape') closeLightbox();
+});
